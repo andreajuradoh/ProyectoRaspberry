d6ee3e47c2dfb57da050de1432447dca6fb64b4d
Andrea Jurado <avjh_18@hotmail.com>
2017-09-11 14:43:52 -0500

    ghefwh

---
+++ UserTest.php
@@ -0,0 +1,13 @@
+  GNU nano 2.5.3             Archivo: UserTest.php                                   
+
+<?php
+require_once "User.php";
+use PHPUnit\Framework\TestCase;
+class UserTest extends TestCase{
+  public function testTalk() {
+    $user = new User();
+    $expected = "Hello world!35";
+    $actual = $user->talk();
+    $this->assertEquals($expected, $actual);
+  }
+}
