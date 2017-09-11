716661b9200f32b61b9efa5de704541208608795
Andrea Jurado <avjh_18@hotmail.com>
2017-09-11 14:25:30 -0500

    dos

---
+++ DependsTest.php
@@ -0,0 +1,16 @@
+<?
+class DependsTest extends _Framework_TestCase
+{
+    public function testEmpty()
+    {
+        $stack = array();
+        $this->assertEmpty($stack,0);
+        
+        ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+        return $stack;
+    }
+    
+}
